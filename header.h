a879213b67cfd178ce7843406c8cbd6211807822
Kozak Mikhail <kozak_mikhail.1@mail.ru>
2025-09-05 00:37:08 +0300

    Change array on the vectors

--- header.h
+++ header.h
@@ -1,13 +1,15 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 class Matrix
 {
     private:
-    int number_of_strs;
+    int number_of_rows;
     int number_of_columns;
 
-    int **matrix = nullptr;
+    vector<vector<int>> matrix;
 
     public:
     int get_numbers_strs ()const
@@ -16,7 +18,7 @@ class Matrix
     }
     int get_numbers_columns ()const
     {
-        return number_of_strs;
+        return number_of_rows;
     }
 
     int get_matrix_value(int str_index, int column_index) const
@@ -24,35 +26,24 @@ class Matrix
         return matrix[str_index][column_index];
     }
 
-    Matrix() : number_of_columns (2), number_of_strs(2)
+   Matrix() : number_of_rows(2), number_of_columns(2)
     {
-        matrix = new int* [number_of_strs];
-        for(int i = 0;i < number_of_columns;i++)
-        {
-            matrix[i] = new int[number_of_columns]();
-        }
+        matrix.resize(number_of_rows, vector<int>(number_of_columns, 0));
     }
-    Matrix(int rang) : number_of_columns(rang), number_of_strs(rang)
+    
+    Matrix(int rang) : number_of_rows(rang), number_of_columns(rang)
     {
-        matrix = new int* [number_of_strs];
-        for(int i = 0;i < number_of_columns;i++)
-        {
-            matrix[i] = new int[number_of_columns]();
-        }
+        matrix.resize(number_of_rows, vector<int>(number_of_columns, 0));
     }
 
-    Matrix(int number_of_strs, int number_of_columns) : number_of_strs(number_of_strs), number_of_columns(number_of_columns)
+    Matrix(int rows, int columns) : number_of_rows(rows), number_of_columns(columns)
     {
-        matrix = new int* [number_of_strs];
-        for(int i = 0;i < number_of_columns;i++)
-        {
-            matrix[i] = new int[number_of_columns]();
-        }
+        matrix.resize(number_of_rows, vector<int>(number_of_columns, 0));
     }
 
     void Create()
     {
-        for(int i = 0;i < number_of_strs;i++)
+        for(int i = 0;i < number_of_rows;i++)
         {
             cout << "Enter "<< i + 1 <<"-th line of matrix" << endl;
             for(int j  = 0;j < number_of_columns;j++)
@@ -65,7 +56,7 @@ class Matrix
 
     void Show() const
     {
-        for(int i = 0;i < number_of_strs;i++)
+        for(int i = 0;i < number_of_rows;i++)
         {
             for(int j = 0;j < number_of_columns;j++)
             {
@@ -78,9 +69,9 @@ class Matrix
 
     void Subtraction(Matrix &subtractor)
     {
-        if(subtractor.get_numbers_columns() == number_of_columns && subtractor.get_numbers_strs() == number_of_strs)
+        if(subtractor.get_numbers_columns() == number_of_columns && subtractor.get_numbers_strs() == number_of_rows)
         {
-            for(int i = 0;i < number_of_strs;i++)
+            for(int i = 0;i < number_of_rows;i++)
             {
                 for(int j = 0;j < number_of_columns;j++)
                 {
@@ -92,10 +83,5 @@ class Matrix
         {
             cout << "Error, Matrixs are not equal\n";
         }
-    }
-
-    ~Matrix()
-    {
-        delete[] matrix;
     }
 };
